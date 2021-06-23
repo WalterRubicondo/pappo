d0e3276e1d434a78bb9ba22369ad74d3305df308
giuseppe <placidagiuseppe@gmail.com>
2021-06-23 10:23:53 +0200

    non pullate

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -12,6 +12,34 @@
         </div>
         <div class="r">
             <img src="./img/r.svg" alt="">
+              <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ route('admin.index') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Pappo
+                </div>
+
+                <div class="links">
+
+                </div>
+            </div>
         </div>
     </div>
     <div class="title">
